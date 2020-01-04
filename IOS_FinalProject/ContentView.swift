d4f26696710391f69f27253cfe024a234392cc74
jasper <cool.daniel1234@gmail.com>
2020-01-04 21:10:59 +0800

    Initial Commit

---
+++ IOS_FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_FinalProject
+//
+//  Created by User02 on 2020/1/4.
+//  Copyright © 2020 matcha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
